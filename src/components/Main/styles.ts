a66d42f7ae6be5352653f62c24d78ab63addfd85
aldo-rl <aldorl.mee@gmail.com>
2022-08-07 11:48:28 -0500

    [UPDATE] style main separation botto

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -5,6 +5,7 @@ const { StatusBarManager } = NativeModules;
 export const styles = StyleSheet.create({
   main: {
     ...StyleSheet.absoluteFillObject,
+    paddingBottom: 32,
   },
   content: {
     paddingTop: StatusBarManager.HEIGHT + 32,
